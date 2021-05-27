3525cffc739deddd9d203804b72b33ef6eea6855
totalnemtom <matybal.m@gmail.com>
2021-05-27 19:33:43 +0200

    array vs arraylist

--- src/lesson/ArrayVsArrayList.java
+++ src/lesson/ArrayVsArrayList.java
@@ -7,7 +7,13 @@ public class ArrayVsArrayList {
         String[] simpleArray = new String[2];
         simpleArray[0] = "apple";
         simpleArray[1] = "peach";
+        System.out.println("simpleArray méret: " + simpleArray.length);
 
         ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("peach");
+        System.out.println("első elem: " + list.get(0) + " méret: " + list.size());
+        list.remove(0);
+        System.out.println("első elem: " + list.get(0) + " méret: " + list.size());
     }
 }
