2b018613e6bc8c7e0c9b8bd4410d705685976d8e
totalnemtom <matybal.m@gmail.com>
2021-05-27 19:23:23 +0200

    repo rename

---
+++ src/lesson/ArrayVsArrayList.java
@@ -0,0 +1,13 @@
+package lesson;
+
+import java.util.ArrayList;
+
+public class ArrayVsArrayList {
+    public static void main(String[] args) {
+        String[] simpleArray = new String[2];
+        simpleArray[0] = "apple";
+        simpleArray[1] = "peach";
+
+        ArrayList<String> list = new ArrayList<>();
+    }
+}
